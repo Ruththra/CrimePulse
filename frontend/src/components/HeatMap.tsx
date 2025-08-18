48d8ba0fd8d5473ef9f343bf47148d6fb4895ba4
Ruththra <ruththu2003@gmail.com>
2025-08-19 02:23:11 +0530

    fix: Heatmap error fixed

--- frontend/src/components/HeatMap.tsx
+++ frontend/src/components/HeatMap.tsx
@@ -1,5 +1,5 @@
-// <reference types="vite/client" />
-// <reference types="@types/google.maps" />
+/// <reference types="vite/client" />
+/// <reference types="@types/google.maps" />
 import { useEffect, useRef, useState } from 'react';
 import { Loader } from '@googlemaps/js-api-loader';
 import { TrendingUp } from 'lucide-react';
